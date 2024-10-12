aa40b7f854a4b974aafb9408b92db16f82756e37
peterodejobi <peter.odejobi@prunny.com>
2024-10-12 16:02:44 +0100

    Add App fonts.

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,10 +1,8 @@
 export const fonts = {
-    family: {
-        "Roboto-Bold": require('../assets/fonts/Roboto-Bold.ttf'),
-        "Roboto-Light": require('../assets/fonts/Roboto-Light.ttf'),
-        "Roboto-Medium": require('../assets/fonts/Roboto-Medium.ttf'),
-        "Robot-Regular": require('../assets/fonts/Roboto-Regular.ttf'),
-    }
+    bold: require('../assets/fonts/Roboto-Bold.ttf'),
+    light: require('../assets/fonts/Roboto-Light.ttf'),
+    medium: require('../assets/fonts/Roboto-Medium.ttf'),
+    regular: require('../assets/fonts/Roboto-Regular.ttf'),
 }
 
-export type Fonts = keyof typeof fonts.family;
+export type Fonts = keyof typeof fonts;
