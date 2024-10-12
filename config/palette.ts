ac3e52642bbb4ce423d35834f748a22cb47f4793
peterodejobi <peter.odejobi@prunny.com>
2024-10-12 23:33:39 +0100

    Add color palette.

--- config/palette.ts
+++ config/palette.ts
@@ -1,16 +1,17 @@
 export const palette = {
-    "primary--1": "#4E157B",
+    "primary--1": "#2D1145",
     "primary--2": "#C5A4E0",
-    "primary--3": "#C378FF12", // opaque .7%
+    "primary--3": "#2D11450F",
     "primary--4": "#FAF5FF",
     "grey--1": "#444444",
-    "grey--2": "#2D11450F",
+    "grey--2": "#2D1145CC",
     "grey--3": "#C5C5C5",
-    "red": "#EB5757",
-    "yellow": "#FDB713",
+    red: "#EB5757",
+    yellow: "#FDB713",
     "green--1": "#27AE60",
     "green---2": "#FDB713",
-    "black": "#000000",
+    black: "#000000",
+    white: "#FFFFFF"
 };
 
 export type Palette = keyof typeof palette;
