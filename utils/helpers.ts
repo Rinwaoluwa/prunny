57fe05b302c8bf69b5af37c335d26fbb3e28d9a7
peterodejobi <peter.odejobi@prunny.com>
2024-10-18 01:37:29 +0100

    Add helper function

---
+++ utils/helpers.ts
@@ -0,0 +1,8 @@
+export const formatDate = (date: Date) => {
+    if(!date) return;
+
+    let day = date.getDate().toString().padStart(2, '0'); // Add leading 0
+    let month = (date.getMonth() + 1).toString().padStart(2, '0'); // Add leading 0, getMonth() is 0-indexed
+    let year = date.getFullYear();
+    return `${day} - ${month} - ${year}`;
+};
