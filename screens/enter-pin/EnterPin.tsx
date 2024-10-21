f3bb1a721826e8397f15595b7ff1c4238f5ecd52
peterodejobi <peter.odejobi@prunny.com>
2024-10-21 13:41:32 +0100

    refactor: Make EnterPin Screen a reuseable component.

--- screens/enter-pin/EnterPin.tsx
+++ screens/enter-pin/EnterPin.tsx
@@ -2,16 +2,26 @@ import { AppText } from "@/components/AppText";
 import OtpInput from "@/components/OtpInput";
 import { FLEX } from "@/config/constants";
 import { useState } from "react";
-import { View } from "react-native";
+import { View, ViewStyle } from "react-native";
 import { styles } from "./style";
 import { Button } from "@/components/buttons/Button";
-import { heightPixel, widthPixel } from "@/config/normalise";
+import { heightPixel, pixelSizeVertical, widthPixel } from "@/config/normalise";
 
 interface Props {
     handleContinue: () => void;
+    title?: string;
+    caption?: string;
+    style?: ViewStyle;
+    buttonTitle?: string;
 }
 
-export default function EnterPin({ handleContinue }: Props) {
+export default function EnterPin({
+    title,
+    caption = "Please enter your PIN to complete transaction",
+    style,
+    buttonTitle = "Proceed",
+    handleContinue
+}: Props) {
     const [pin, setPin] = useState("");
     const handleSetPin = (pin: string) => {
         setPin(pin);
@@ -20,21 +30,31 @@ export default function EnterPin({ handleContinue }: Props) {
         }
     }
     return (
-        <View style={[FLEX, styles.container]}>
+        <View style={[FLEX, styles.container, style]}>
+            {title && (
+                <AppText
+                    fontSize={18}
+                    color="primary--1"
+                    fontFamily="bold"
+                    style={styles.title}
+                >
+                    {title}
+                </AppText>
+            )}
             <AppText
                 color="grey--2"
                 fontFamily="regular"
                 style={styles.textStyle}
             >
-                Please enter your PIN to complete transaction
+                {caption}
             </AppText>
             <OtpInput
                 length={6}
-                inputStyle={{ width: widthPixel(49.8), height: heightPixel(44) }}
+                inputStyle={{ width: widthPixel(49.8), height: heightPixel(44), marginRight: pixelSizeVertical(4.24) }}
                 onComplete={handleSetPin}
             />
             <View style={FLEX} />
-            <Button title="Proceed" backgroundColor="primary--4" onPress={handleContinue} />
+            <Button title={buttonTitle} backgroundColor="primary--4" onPress={handleContinue} />
         </View>
     )
 }
