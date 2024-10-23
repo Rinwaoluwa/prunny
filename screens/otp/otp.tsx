4b0077beefbd29caa81dd6affea7690490f50196
peterodejobi <peter.odejobi@prunny.com>
2024-10-23 15:45:28 +0100

    otp: Add form validation.

--- screens/otp/otp.tsx
+++ screens/otp/otp.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Pressable, View } from "react-native";
+import { z } from "zod";
 import { styles } from "./styles";
 import { FLEX } from "@/config/constants";
 import { AppText } from "@/components/AppText";
@@ -9,6 +10,9 @@ import useCountdownTimer from "@/hooks/useCountdownTimer";
 import Icon from "@/assets/svgs/icons";
 import { useOtp } from "@/hooks/useFakeOtp";
 import { Button } from "@/components/buttons/Button";
+import { otpSchema } from "@/config/schema/schema";
+
+type OTP_Z_Type = z.infer<typeof otpSchema>;
 
 export default function OTP({ title, handleContinue }: { title: string, handleContinue: () => void }) {
 
@@ -20,12 +24,36 @@ export default function OTP({ title, handleContinue }: { title: string, handleCo
         start(60) // start count down;
     }, []);
 
+    const [error, setError] = useState<Partial<OTP_Z_Type['otp']>>("");
+
+    const validateForm = (): boolean => {
+        try {
+            otpSchema.parse({ otp: otpValue });
+            setError("");
+            return true;
+        } catch (error) {
+            if (error instanceof z.ZodError) {
+                setError(error.errors[0].message);
+            }
+            return false;
+        }
+    };
+
     const handleSetOtpValue = (otp: string) => {
         setOtpValue(otp);
-        if (otp) {
-            handleContinue();
-        }
     }
+
+    const handleSubmit = () => {
+        const validOtp = otpValue === "1234" ? true : otpValue === "4321" ? true : false;
+        if (validateForm()) {
+            if (!validOtp) {
+                setError("Invalid Otp!");
+                setOtpValue("");
+            } else {
+                handleContinue();
+            }
+        }
+    };
 
     return (
         <>
@@ -54,6 +82,16 @@ export default function OTP({ title, handleContinue }: { title: string, handleCo
                 </AppText>
             </View>
             <OtpInput length={4} onComplete={handleSetOtpValue} />
+            {error && (
+                <AppText
+                    fontSize={12}
+                    color="red"
+                    fontFamily="medium"
+                    style={{ marginVertical: pixelSizeVertical(8) }}
+                >
+                    {error}
+                </AppText>
+            )}
             <Pressable
                 onPress={() => {
                     resend();
@@ -75,7 +113,7 @@ export default function OTP({ title, handleContinue }: { title: string, handleCo
             <Button
                 title="Continue"
                 backgroundColor="primary--4"
-                onPress={handleContinue}
+                onPress={handleSubmit}
                 disabled={Boolean(!otpValue)}
                 style={{ marginBottom: pixelSizeHorizontal(44) }}
             />
