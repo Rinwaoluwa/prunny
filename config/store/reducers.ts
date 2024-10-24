4776a92ece1723a21b9c0f4e75324995ff990ef5
peterodejobi <peter.odejobi@prunny.com>
2024-10-24 13:45:05 +0100

    login: Add global state & form validation.
    refactor: Update PHONE_REGEX to receive 10 - 11 digit numbers.

--- config/store/reducers.ts
+++ config/store/reducers.ts
@@ -2,25 +2,18 @@ import { combineReducers } from "@reduxjs/toolkit";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { persistReducer } from "redux-persist";
 import signUpReducer from "./slices/signUpSlice";
+import profileInfoReducer from "./slices/profileInfoSlice";
 
 const rootReducer = combineReducers({
   signUp: signUpReducer,
-//   resetPassword: resetPasswordReducer,
-//   profileInfo: profileInfoReducer,
-//   bankList: bankListSlice,
-//   kycInfo: kycInfoReducer,
-//   appSettings: appSettingsReducer,
-//   transaction: transactionSlice,
-//   billerCategories: billerCategoryReducer,
-//   cards: cardSlice,
-//   notifications: notificationSlice
+  profileInfo: profileInfoReducer,
 });
 
 const persistConfig = {
   key: "root",
   storage: AsyncStorage,
-//   whitelist: ["profileInfo", "kycInfo", "transaction", "appSettings", "cards",],
-//   blacklist: ["signUp", "resetPassword", "billerCategories", "bankList"],
+  whitelist: ["profileInfo", "transaction", "cards",],
+  blacklist: ["signUp", "resetPassword"],
   version: 1,
 };
 
